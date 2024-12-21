cdf331c2d33be8ad24f1764202f3faa447d66823
Ризобек Бабажанов <nazimaibragimova7582@gmail.com>
2024-12-21 16:57:26 +0500

    Review

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.review__carousel').slick({
+        infinite: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+    })
+});
