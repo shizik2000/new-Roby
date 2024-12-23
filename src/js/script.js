b7d6760eecb877017a564c25ef051221976ec6db
Ризобек Бабажанов <nazimaibragimova7582@gmail.com>
2024-12-23 20:14:37 +0500

    Media (There are bugs there)

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,35 @@ $(document).ready(function() {
         centerPadding: '60px',
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: false
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              centerMode: false
+            }
+          }
+        ]
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelector('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
     });
 
     new WOW().init();
